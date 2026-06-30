64ea0f0a4cfd7fa2c3b658c4494a86ced4162a2c
sakshi1219 <sakshigole303@gmail.com>
2026-06-30 23:44:33 +0530

    Update the model and streamlit app

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 import streamlit as st
+import joblib
 
-st.title("Retail Profitability Prediction")
+model = joblib.load("D:/Project/Supermarket-Profit-Prediction/Models/logistic_model.pkl")
+scaler = joblib.load("D:/Project/Supermarket-Profit-Prediction/Models/scaler.pkl")
 
-sales = st.number_input("Enter Sales")
-discount = st.number_input("Enter Discount")
+st.title("Retail Profitability Predictor")
+
+sales = st.number_input("Sales", min_value=0.0)
+discount = st.number_input("Discount", min_value=0.0, max_value=1.0)
 
 if st.button("Predict"):
-    st.write("Prediction goes here")
+
+    input_data = [[sales, discount]]
+
+    # Scale before prediction
+    input_scaled = scaler.transform(input_data)
+
+    prediction = model.predict(input_scaled)
+    probabilities = model.predict_proba(input_scaled)
+
+    st.write("Probabilities:", probabilities)
+
+    if prediction[0] == 1:
+        st.success("✅ Order is likely to be Profitable")
+    else:
+        st.error("❌ Order is likely to be Not Profitable")
